3070f81a4bccc265ff1d28ae3e574363ba7500d9
amrrs <amrrs@outlook.com>
2018-05-10 01:02:28 +0530

    fix description note cran check

--- R/tiobieindexr-package.R
+++ R/tiobieindexr-package.R
@@ -1,4 +1,4 @@
-#' @title Extract 'TIOBE' Index Tables for the latest month
+#' @title Extract 'TIOBE' Programming Index Tables for the latest month
 #' @description Get latest TIOBE Index Tables. The TIOBE Programming Community index
 #'              is an indicator of the popularity of programming languages
 #'              from <https://www.tiobe.com/tiobe-index/>
