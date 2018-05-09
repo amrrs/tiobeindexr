f85383fcd65aced79351820494f66d3e7857541c
amrrs <amrrs@outlook.com>
2018-05-10 01:12:03 +0530

    added see also

--- R/tiobieindexr-package.R
+++ R/tiobieindexr-package.R
@@ -3,6 +3,12 @@
 #'              is an indicator of the popularity of programming languages
 #'              from <https://www.tiobe.com/tiobe-index/>
 #'
+#' @section See Also:
+#' Useful links:
+#' * [https://www.tiobe.com/tiobe-index/](https://www.tiobe.com/tiobe-index/)
+#' * [https://github.com/amrrs/tiobeindexr](https://github.com/amrrs/tiobeindexr)
+#' * Report bugs at [https://github.com/amrrs/tiobeindexr/issues](https://github.com/amrrs/tiobieindexr/issues)
+#'
 #' @md
 #' @name tiobieindexr
 #' @docType package
